9d0050019ca0ff59c08835b012f649cbb36f5a0f
Anisul Mahmud <first.last@tuni.fi>
2024-04-25 12:57:58 +0300

    final commit

---
+++ backend/server-a/controllers/Order.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var OrderService = require('../service/OrderService');
+var sendTask = require('../rabbit-utils/sendTask.js');
+
+function generateUniqueIntegerId() {
+  let id =0
+
+  while(id == 0) {
+    id = Math.floor(Math.random() * 1000);
+  }
+
+  return id;
+}
+
+
+
+module.exports.addOrder = function addOrder(req, res, next) {
+  
+  const oID = generateUniqueIntegerId();
+  var order = {
+    
+    sandwichId: 1,
+    id: oID,
+    status: "ordered"
+  };
+
+  OrderService.addOrder(order)
+    .then(function (response) {
+      utils.writeJson(res, response);
+      // sendTask.addTask("rapid-runner-rabbit", "received-orders", order);
+    })
+    .catch(function (error) {
+      utils.writeJson(res, error);
+    });
+};
+
+module.exports.getOrderById = function getOrderById(req, res, next) {
+  var orderId = req.swagger.params['orderId'].value;
+  OrderService.getOrderById(orderId)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (error) {
+      utils.writeJson(res, error);
+    });
+};
+
+module.exports.getOrders = function getOrders(req, res, next) {
+  OrderService.getOrders()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (error) {
+      utils.writeJson(res, error);
+    });
+};
---
+++ backend/server-a/utils/db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const options = {
+            useNewUrlParser: true,
+        };
+        const connection = await mongoose.connect('mongodb://mongo:27017/SandwichDb', options ); 
+        if (connection){
+            console.log('Connected to MongoDB');
+        }
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+    }
+
+module.exports = { connectDB };
